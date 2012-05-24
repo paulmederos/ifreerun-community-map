70f34bf0942bad990d11bb772243e05de80e8c40
Paul Mederos <paul@primeenterprise.net>
2012-05-24 17:45:40 -0400

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-ifreerun-community-map
-======================
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ifreerun::Application.config.secret_token = '1beb2db7f9a2735f11203813941e80b17e67a27cb97f2aa5e517856e0e1cd212dc75c21bfc44032d8d1f7e3412ceae82faa8c50c2453170fb5597cb0490caf34'
