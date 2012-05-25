99217876f539579a031747ad6cf0063ca6f04627
Paul Mederos <paul@primeenterprise.net>
2012-05-24 23:48:29 -0400

    PostgreSQL running locally. Remember to configure database.yml and local PG server. Also added root route (non-functional ATM) and removed the public index.

--- Gemfile
+++ Gemfile
@@ -1,11 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.1'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
-gem 'sqlite3'
+gem 'thin'
+gem 'pg'
 
 
 # Gems used only for assets and not required
@@ -15,25 +12,15 @@ group :assets do
 	gem 'less', '~> 2.2.1'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+	gem 'therubyracer'
 end
 
-group :development do
+group :development, :test do
 	gem 'nifty-generators'
+	gem 'rspec', '~> 2.8'
+	gem 'ruby-debug19', :require => 'ruby-debug'
 end
+
 
 gem 'jquery-rails'
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the web server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'ruby-debug19', :require => 'ruby-debug'
+gem 'rails-backbone'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Ifreerun::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'pages#home'
+ 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
@@ -45,14 +46,4 @@ Ifreerun::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
