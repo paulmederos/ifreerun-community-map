b309f8c7c92b19058ca3ad986ce834f51e1fe1ec
Paul Mederos <paul@primeenterprise.net>
2012-05-28 14:33:53 -0400

    Basic authentication from scratch (following RBates #250) with signup, login, and logout. Next up is cookies!

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   
   validates_uniqueness_of :email
+  #validate proper email, validate length of password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Ifreerun::Application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   
   resources :users
+  resources :sessions
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
