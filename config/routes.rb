0999407c9634141b9486b2f143c7cd103ef9f291
Paul Mederos <paul@primeenterprise.net>
2012-05-28 14:50:26 -0400

    Forgot password controller added. Mailer for sending forgotten password added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Ifreerun::Application.routes.draw do
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
   resources :users
   resources :sessions
+  resources :password_resets
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120528183437) do
+ActiveRecord::Schema.define(:version => 20120528184437) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
     t.string   "auth_token"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
