3e6d7b074a1ea9d790e44668c77ae67998e31547
Paul Mederos <paul@primeenterprise.net>
2012-06-19 23:34:31 -0400

    Moved map to it's own page. New home page started.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Ifreerun::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'pages#home'
+  
+  match '/map' => 'pages#map'
  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
